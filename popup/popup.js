0ca34b4e2045049cf5e5fce19f492d8c9f9af53e
yhdeon <deonprojdev@hotmail.com>
2025-02-01 14:57:09 +0800

    Merge chris portion with mine - updated background.js, popup.html and popup.js

--- popup/popup.js
+++ popup/popup.js
@@ -1,38 +1,48 @@
 // test - detecting <input> fields for email and password
 document.getElementById("Scan").addEventListener("click", () => {
-    // Query the active tab in the current window
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        if (tabs.length > 0) {
-            // Execute a script in the context of the current tab
-            chrome.scripting.executeScript({
-                target: { tabId: tabs[0].id },
-                func: () => {
-                    function detectInput() {
-                      var input_emailfield = document.querySelectorAll('input[name="email"]');
-                      if (input_emailfield.length > 0) {
-                        console.log("Input field detected");
-                        console.log("No. of input fields: " + input_emailfield.length);
-                      } else {
-                        console.log("No input field detected");
-                      }
-                    };
+  // Query the active tab in the current window
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    if (tabs.length > 0) {
+      const [tab] = /*await*/ chrome.tabs.query({ active: true, currentWindow: true });
+      const url = tab.url;
 
-                    function detectPwdInput() {
-                      var input_pwdfield = document.querySelectorAll('input[name="password"]');
-                      if (input_pwdfield.length > 0) {
-                        console.log("Password field detected");
-                        console.log("No. of password fields: " + input_pwdfield.length);
-                      } else {
-                        console.log("No password field detected");
-                      }
-                    };
+      // Execute a script in the context of the current tab
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        func: () => {
+          function detectInput() {
+            var input_emailfield = document.querySelectorAll('input[name="email"]');
+            if (input_emailfield.length > 0) {
+              console.log("Input field detected");
+              console.log("No. of input fields: " + input_emailfield.length);
+            } else {
+              console.log("No input field detected");
+            }
+          };
 
-                    detectInput();
-                    detectPwdInput();
-                },
-            });
-        } else {
-            console.error("No active tab found.");
-        }
-    });
+          function detectPwdInput() {
+            var input_pwdfield = document.querySelectorAll('input[name="password"]');
+            if (input_pwdfield.length > 0) {
+              console.log("Password field detected");
+              console.log("No. of password fields: " + input_pwdfield.length);
+            } else {
+              console.log("No password field detected");
+            }
+          };
+
+          detectInput();
+          detectPwdInput();
+        },
+      });
+      //output result of url check - chris
+      chrome.runtime.sendMessage({ action: "scan", url }, (response) => {
+        const resultDiv = document.getElementById("result");
+        resultDiv.innerHTML = `
+                  <p><strong>Heuristic Check:</strong> ${response.heuristicCheck}</p>
+              `;
+      });
+    } else {
+      console.error("No active tab found.");
+    }
+  });
 });
